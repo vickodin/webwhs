6f6b3ff15b554fb1c7df2f534e1752c052b622d8
kunashir <kunashir@list.ru>
2012-12-25 22:00:01 +0400

    create empty app and add foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webwhs::Application.config.secret_token = '15dcdf1f3062363866d8902711e97ddef99b7c22ef296e7c30d57f011e4d91ad9d72c7fdaf88d2c1075025a1a08700121cc3f6eb978bad1824f9c1c2c4b530fe'
